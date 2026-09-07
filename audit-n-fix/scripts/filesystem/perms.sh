1525cdf5d72ec363b57d0ccfca59719a627d6868
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-09-07 17:10:13 -0500

    Fix indentation for `case` statement.

--- audit-n-fix/scripts/filesystem/perms.sh
+++ audit-n-fix/scripts/filesystem/perms.sh
@@ -27,30 +27,30 @@ select_fix() {
 		# Validate given user and group
 		# Change the ownership
 		case "${selection}" in
-			'Change ownership')
-				until [[ "${user}" =~ ^[0-9]+$ ]] || getent passwd -- "${user}" &>/dev/null && [[ -n "${user}" ]]; do
-					[[ -n "${x}" ]] && log w 'Invalid username/UID provided.' || x=true
-					read -rp 'Enter the new user owner: ' user
-				done
-				until [[ "${group}" =~ ^[0-9]+$ ]] || getent group -- "${group}" &>/dev/null && [[ -n "${group}" ]]; do
-					[[ -n "${y}" ]] && log w 'Invalid group/GID provided.' || y=true
-					read -rp 'Enter the new group owner: ' group
-				done
-				chown -hc -- "${user}:${group}" "${1}"
-			;;
-			'Change permissions')
-				until [[ "${perm}" =~ ^[1234567]{3,4}$ ]]; do
-					read -rp 'Enter the octal permission: ' perm
-				done
-				chmod -c -- "${perm}" "${1}"
-			;;
-			'Rename node')
-				read -rp 'Enter the new name for the node' basename
-				mv -- "${1}" "$(dirname -- "${1}")""${basename}"
-			;;
-			'Delete node')
-				rm -rfv -- "${1}"
-			;;
+		'Change ownership')
+			until [[ "${user}" =~ ^[0-9]+$ ]] || getent passwd -- "${user}" &>/dev/null && [[ -n "${user}" ]]; do
+				[[ -n "${x}" ]] && log w 'Invalid username/UID provided.' || x=true
+				read -erp 'Enter the new user owner: ' user
+			done
+			until [[ "${group}" =~ ^[0-9]+$ ]] || getent group -- "${group}" &>/dev/null && [[ -n "${group}" ]]; do
+				[[ -n "${y}" ]] && log w 'Invalid group/GID provided.' || y=true
+				read -erp 'Enter the new group owner: ' group
+			done
+			chown -hc -- "${user}:${group}" "${1}"
+		;;
+		'Change permissions')
+			until [[ "${perm}" =~ ^[1234567]{3,4}$ ]]; do
+				read -erp 'Enter the octal permission: ' perm
+			done
+			chmod -c -- "${perm}" "${1}"
+		;;
+		'Rename node')
+			read -erp 'Enter the new name for the node' basename
+			mv -- "${1}" "$(dirname -- "${1}")""${basename}"
+		;;
+		'Delete node')
+			rm -rfv -- "${1}"
+		;;
 		esac
 	done
 }
