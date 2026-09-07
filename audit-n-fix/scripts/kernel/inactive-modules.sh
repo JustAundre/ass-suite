34575d51e91b3b093a2c04f024c1c816e83289e8
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-09-07 17:11:25 -0500

    Modernize loops.
    Added guard for missing kernel modules for current active kernel (occurs when updating the kernel but sysadmin(s) have not restarted).

--- audit-n-fix/scripts/kernel/inactive-modules.sh
+++ audit-n-fix/scripts/kernel/inactive-modules.sh
@@ -8,13 +8,17 @@
 # Inactive Module Management
 #
 # Block all currently inactive modules from ever loading.
-confirm 'Prevent currently unused kernel modules from ever being loaded' && while IFS= read -r mod; do
-	echo "install ${mod} /bin/false" >>/etc/modprobe.d/hardening.conf
+if [[ ! -d /lib/modules/$(uname -r) ]]; then
+	log e 'The kernel modules for your active kernel no longer exist on the disk.' 'Please restart your machine.'
+	exit 10
+fi
 #
-# uniq -u: If a line occurs more than once, all instances of that line are removed.
-done < <(sort < <(
-	# Combine results from active modules and ALL modules.
-	find /lib/modules/"$(uname -r)" -type f -name '*.ko*' -print0 | xargs -0n1 basename | cut -d. -f1
-	grep -Eo '^[^ ]+' /proc/modules
+# Subtract list of active modules from list of all modules to get list of inactive modules.
+mapfile -td '' mods  < <(sort < <(
+	find "/lib/modules/$(uname -r)" -type f -name '*.ko*' -printf '%f\n' | cut -d '.' -f1
+	grep -oE '^\w+' /proc/modules | sed 's/_/-/g'
 ) | uniq -u)
-log i 'You can find blocked kernel modules @ "/etc/modprobe.d/hardening.conf"'
+confirm 'Prevent currently unused kernel modules from ever being loaded' && for mod in "${mods[@]}"; do
+	echo "install ${mod} /bin/false" >>/etc/modprobe.d/hardening.conf
+done
+log i 'The new blacklist can be found at "/etc/modprobe.d/hardening.conf".'
