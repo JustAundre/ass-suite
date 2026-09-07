a6f5476d88a35d92967e9b7415044a808f8c6601
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-09-07 17:11:35 -0500

    Modernize loops.

--- audit-n-fix/scripts/software/repo.sh
+++ audit-n-fix/scripts/software/repo.sh
@@ -11,13 +11,14 @@
 mapfile -td '' repo_files < <(find /etc/yum.repos.d/ /etc/apt/sources.list.d/ /etc/apt/sources.list -mindepth 1 ! -type d -print0)
 for repo_file in "${repo_files[@]}"; do
 	# Loop over every line URL in the repositories file
-	while read -r repo_url; do
+	mapfile -td '' urls < <(grep -oE 'https?://[^/ ]+' "${repo_file}" | sort -z | uniq -zu)
+	for url in "${urls[@]}"; do
 		# Compare the domains of the newly found URL with the URL from the /etc/os-release file.
 		# If the domains match, log it; otherwise, flag it.
-		[[ -n "${repo_url}" ]] && if compdom "${os_info[HOME_URL]}" "${repo_url}"; then
-			log i "URL \"${repo_url}\" was identified."
+		[[ -n "${url}" ]] && if compdom "${os_info[HOME_URL]}" "${url}"; then
+			log i "URL \"${url}\" was identified."
 		else
-			log w "URL with foreign domain \"${repo_url}\" found in file \"${repo_file}\"."
+			log w "URL with foreign domain \"${url}\" found in file \"${repo_file}\"."
 		fi
-	done < <(grep -oE 'https?://[^/ ]+' "${repo_file}" | sort -ux)
+	done
 done
