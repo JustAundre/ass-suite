05e0773baf576eedc04b3cbb105e6b18be62254c
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-09-14 19:55:26 -0500

    QOL: Update `audit-n-fix/scripts/cron/crontab.sh` to skip empty crontabs

--- audit-n-fix/scripts/cron/crontab.sh
+++ audit-n-fix/scripts/cron/crontab.sh
@@ -7,12 +7,15 @@
 #
 # Crontabs
 #
-# Prompt to review/edit the crontab of each user
-# Prompt to delete the crontab of each user
+# Prompt to review, edit and/or delete the crontab of every user
 hash crontab &>/dev/null || exit 10
 for u in "${all_users[@]}"; do
 	log i "Reviewing crontab of user: \"${u}\"..."
-	pause 3
-	crontab -eu "${u}"
-	crontab -riu "${u}"
+	if crontab -u "${u}" -l &>/dev/null; then
+    	pause 3
+    	crontab -eu "${u}"
+    	crontab -riu "${u}"
+	else
+		log i "No crontab found for user: \"${u}\". Skipping."
+	fi
 done
